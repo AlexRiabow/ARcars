ac86610abb8777004d5fe002d1db7daf1b152a5e
Alexsandr Ryabow <morboaport.ru@mail.ru>
2023-10-17 22:39:44 +0400

    feat: add form sending + design fixes

---
+++ sendmail.php
@@ -0,0 +1,44 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer-master/src/Exception.php';
+    require 'PHPMailer-master/src/PHPMailer.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('ru','PHPMailer-master/language/');
+    $mail->IsHTML(true);
+
+    $mail->setFrom('info@arcars-samara.ru','ARCars');
+    $mail->addAddress('alexriabow1325@mail.ru');
+    $mail->Subject = 'Заявка с сайта';
+
+    $body = '<h1>Новая заявка с сайта!</h1>';
+
+    if(trim(!empty($_POST['name']))){
+        $body.='<p><strong>Имя:</strong>'.$_POST['name'].'</p>';
+    }
+    if(trim(!empty($_POST['email']))){
+        $body.='<p><strong>Почта:</strong>'.$_POST['email'].'</p>';
+    }
+    if(trim(!empty($_POST['tel']))){
+        $body.='<p><strong>Телефон:</strong>'.$_POST['tel'].'</p>';
+    }
+    if(trim(!empty($_POST['comment']))){
+        $body.='<p><strong>Комментарий:</strong>'.$_POST['comment'].'</p>';
+    }
+
+    $mail->Body = $body;
+
+    if(!$mail->send()){
+        $message = 'Ошибка!';
+    } else {
+        $message = 'Данные отправлены';
+    }
+
+    $response = ['message'=>$message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+?>
